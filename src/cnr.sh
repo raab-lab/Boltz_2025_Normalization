f15c35e8eeb93f7b5a74121e5ddf15c8defdf000
Julia Boltz <122567644+julialindseyboltz@users.noreply.github.com>
2025-06-10 10:10:14 -0400

    Update cnr.sh

--- src/cnr.sh
+++ src/cnr.sh
@@ -13,7 +13,7 @@ module add nextflow
 ## MODIFY THE SAMPLESHEET AND EMAIL FOR YOUR RUN
 nextflow run raab-lab/cut-n-run \
 		--sample_sheet /proj/jraablab/users/jlboltz/Normalization/norm_ss.csv \
-		-profile hg38 \
+		-profile hg38 \ # change genome as needed
 		-w /work/users/j/l/jlboltz \
 		--outdir /proj/jraablab/users/jlboltz/Normalization \
 		-with-report \
