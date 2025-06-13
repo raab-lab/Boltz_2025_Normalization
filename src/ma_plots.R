9d2c0fdae170aca3f3a3b08932b9728a5c141ef4
Julia Boltz <122567644+julialindseyboltz@users.noreply.github.com>
2025-06-13 13:38:37 -0400

    Update ma_plots.R

--- src/ma_plots.R
+++ src/ma_plots.R
@@ -20,3 +20,24 @@ plotMA(k4me3_des_barcodes_shrink, ylim = c(-5, 5), main = "Barcodes MA Plot")
 # E. coli
 k4me3_des_ecoli_shrink <- lfcShrink(k4me3_des_ecoli, coef = 2, type = 'apeglm') 
 plotMA(k4me3_des_ecoli_shrink, ylim = c(-5, 5), main = "Ecoli MA Plot")
+
+################################################################################
+# alternative MA plot that highlights significant upregulated peaks in red and downregulated in blue
+
+res_df <- as.data.frame(k4me3_des_csaw_shrink)
+res_df$regulation <- "Not significant"
+res_df$regulation[res_df$padj < 0.05 & res_df$log2FoldChange > 0] <- "Upregulated"
+res_df$regulation[res_df$padj < 0.05 & res_df$log2FoldChange < 0] <- "Downregulated"
+
+# Plot using ggplot2
+ggplot(res_df, aes(x = baseMean, y = log2FoldChange, color = regulation)) +
+  geom_point(alpha = 0.5) +
+  scale_color_manual(values = c("Upregulated" = "red", "Downregulated" = "blue", "Not significant" = "gray")) +
+  theme_minimal() +
+  theme(
+    panel.grid = element_blank(),               
+    panel.border = element_rect(color = "black", fill = NA, size = 1), 
+    axis.line = element_line(color = "black")   
+  ) +scale_y_continuous(limits = c(-10, 10)) +
+  scale_x_log10(breaks = c(1, 100, 10000)) + # Log scale for x-axis
+  labs(x = "Mean expression", y = "Log2 Fold Change", color = "Regulation")
